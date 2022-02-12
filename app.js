587c44acce4bb7288fca91dd2b66e9c6bd56dc8b
Skronkie <ebrockway01@gmail.com>
2022-02-12 16:08:18 -0600

    added new js file for page-template

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+const profileDataArgs = process.argv.slice(2);
 
-const printProfileData = (profileDataArr) => {
-  for (let i = 0; i < profileDataArr.length; i++) {
-    console.log(profileDataArr[i]);
-  }
+const [profileName, github] = profileDataArgs;
+const generatePage = require("./src/page-template.js");
 
-  console.log("===========");
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-printProfileData(profileDataArgs);
+fs.writeFile("index.html", generatePage(profileName, github), (err) => {
+  if (err) throw new Error(err);
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
